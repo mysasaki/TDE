487ce00234797d4924f511a3461e97c12806e6c2
mysasaki <myllasasaki@gmail.com>
2016-11-18 20:32:05 -0200

    Add

--- JOGO/JOGO/src/GameManager.h
+++ JOGO/JOGO/src/GameManager.h
@@ -1,8 +1,24 @@
 #pragma once
+#include "ofMain.h"
+#include "Hero.h"
+
+#define GAME_MENU 0
+#define GAME_PLAY 1
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
 class GameManager
 {
 public:
-	GameManager();
+	GameManager(int state);
 	~GameManager();
+
+	void Draw();
+	int GetGameState();
+
+private:
+	//ofImage m_mouse;
+	int game_state;
 };
 
